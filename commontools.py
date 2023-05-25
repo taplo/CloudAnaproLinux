299a71c66f95ca7ab7781f355b12ec97ad3c34b8
taplo <taplo@qq.com>
2023-05-25 21:39:56 +0800

    First Commit
    第一次上传

---
+++ commontools.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 04 11:24:18 2019
+
+@author: Administrator
+"""
+
+from numpy import array
+
+from io import StringIO
+from gzip import GzipFile
+
+
+
+def split_list(lst, lenth):
+    '''按照固定长度拆分列表，返回分段索引。'''
+    if lenth>len(lst):
+        return [lst]
+    else:
+        start = array(list(range(0, len(lst), lenth)))
+        end = start + lenth - 1
+        end[-1] = len(lst) - 1
+        pos = list(zip(start, end))
+        result = []
+        for p in pos:
+            result.append(lst[p[0]:p[1]+1])
+            
+        return result
+    
+    
+def gzip_compress(buf):
+    out = StringIO()
+    with GzipFile(fileobj=out, mode='w') as f:
+        f.write(buf)
+    return out.getvalue()
+
+def gzip_decompress(buf):
+    obj = StringIO(buf)
+    with GzipFile(fileobj=obj) as f:
+        result = f.read()
+    return result
+
+
+"""
+调整字符串长度的方法。
+输入字符串和一个整数，将输入字符串长度调整为整数指定的长度，用空格补全。
+如果字符串长度超过指定长度则截断，并在末尾加入...字符。
+"""
+def adjust_string_length(string, length):
+    if len(string) < length:
+        string += ' ' * (length - len(string))
+    elif len(string) > length:
+        string = string[:length-3] + '...'
+    return string
---
+++ redispro2sqlite.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+首次编辑 2019-02-21
+将redis-pro中的数据导入到SQLite数据库中，便于拷贝到笔记本上使用
+@author: Administrator
+"""
+
+import redis
+import pandas as pd
+import sys
+import pprint
+#from wttools.sqlite2kv import ConnectionPool, StrictRedis
+from wtd.tools.sqlite2kv import ConnectionPool, StrictRedis
+
+
+def trans_string(name, r, lr):
+    data = r.get(name)
+    lr.set(name, data)
+
+def trans_hash(name, r, lr):
+    keys = r.hkeys(name)
+    for key in keys:
+        data = r.hget(name, key)
+        lr.hset(name, key, data)
+        #sys.stdout.flush()
+        sys.stdout.write("%s:%d/%d          \r"%(name, keys.index(key), len(keys)))
+
+
+def trans():
+    r = redis.StrictRedis(port=4444, password='rakewang')
+    rpool = ConnectionPool('d:\\sqlite\\default.db')
+    lr = StrictRedis(connection_pool=rpool)
+
+    rkeys = list(r.keys())
+    result = {}
+    for k in rkeys:
+        result[k] = r.type(k)
+
+    print('redis中有如下数据：')
+    pprint.pprint(pd.Series(result))
+
+    print('开始转移数据：')
+    rkeys = list(r.keys())
+    for k in rkeys:
+        if r.type(k)==b'string':
+            trans_string(k, r, lr)
+        elif r.type(k)==b'hash':
+            trans_hash(k, r, lr)
+        else:
+            pass
+        print('%s 完成               '%(k))
+    print('转换完成。')
+    lr.connection_pool.disconnect()
+    r.connection_pool.disconnect()
+    return
+
+
+if __name__ == '__main__':
+
+    r = redis.StrictRedis(port=4444, password='rakewang')
+    rpool = ConnectionPool('d:\\sqlite\\default.db')
+    lr = StrictRedis(connection_pool=rpool)
+
+    rkeys = list(r.keys())
+    result = {}
+    for k in rkeys:
+        result[k] = r.type(k)
+
+    print('redis中有如下数据：')
+    pprint.pprint(pd.Series(result))
+
+    print('开始转移数据：')
+    for k in rkeys:
+        if r.type(k)==b'string':
+            trans_string(k, r, lr)
+        elif r.type(k)==b'hash':
+            trans_hash(k, r, lr)
+        else:
+            pass
+        print('%s 完成               '%(k))
+    print('转换完成。')
+    lr.connection_pool.disconnect()
+    r.connection_pool.disconnect()
