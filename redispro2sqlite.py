0f3b518f3a5ce0be941b2be1070cc8795a94c204
taplo <taplo@qq.com>
2023-06-22 00:41:38 +0800

    脱敏及修正历史错误。

--- redispro2sqlite.py
+++ redispro2sqlite.py
@@ -11,7 +11,7 @@ import sys
 import pprint
 #from wttools.sqlite2kv import ConnectionPool, StrictRedis
 from sqlite2kv import ConnectionPool, StrictRedis
-
+import configparser
 
 def trans_string(name, r, lr):
     data = r.get(name)
@@ -27,8 +27,17 @@ def trans_hash(name, r, lr):
 
 
 def trans():
-    r = redis.StrictRedis(port=4444, password='rakewang')
-    rpool = ConnectionPool('d:\\sqlite\\default.db')
+    conf = configparser.ConfigParser()
+    conf.read('../config.ini',encoding="utf-8-sig")
+    host = conf.get('redis', 'host')
+    port = conf.get('redis', 'port')
+    try:
+        password = conf.get('redis', 'pass')
+    except:
+        password = ""
+
+    r = redis.StrictRedis(host=host, port=port, password=password)
+    rpool = ConnectionPool('/workdir/default.db')
     lr = StrictRedis(connection_pool=rpool)
 
     rkeys = list(r.keys())
@@ -57,27 +66,4 @@ def trans():
 
 if __name__ == '__main__':
 
-    r = redis.StrictRedis(port=4444, password='rakewang')
-    rpool = ConnectionPool('d:\\sqlite\\default.db')
-    lr = StrictRedis(connection_pool=rpool)
-
-    rkeys = list(r.keys())
-    result = {}
-    for k in rkeys:
-        result[k] = r.type(k)
-
-    print('redis中有如下数据：')
-    pprint.pprint(pd.Series(result))
-
-    print('开始转移数据：')
-    for k in rkeys:
-        if r.type(k)==b'string':
-            trans_string(k, r, lr)
-        elif r.type(k)==b'hash':
-            trans_hash(k, r, lr)
-        else:
-            pass
-        print('%s 完成               '%(k))
-    print('转换完成。')
-    lr.connection_pool.disconnect()
-    r.connection_pool.disconnect()
+    trans()
