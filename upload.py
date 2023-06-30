8a9de5550c5ca5357257557ce250a04a789e0ee5
taplo <taplo@qq.com>
2023-06-30 10:27:27 +0800

    修改以适应所有平台

--- upload.py
+++ upload.py
@@ -4,7 +4,7 @@ Created on 2023-05-19 09:10:00
 通过阿里云上传数据库文件
 @author: Administrator
 """
-import os
+import shutil, os
 import datetime
 
 import configparser
@@ -30,13 +30,22 @@ def upload_file():
     # 按照日期复制文件
     filename = datetime.datetime.now().isoformat().split('.')[0] + '.db'
     filename = filename.replace(':', '-')
+    '''旧的linux版复制文件
     cmd = "cp /workdir/default.db /workdir/" + filename
     result = os.system(cmd)
     print(result)
+    '''
+    shutil.copy('/workdir/default.db', '/workdir/'+filename)
+    print('已复制'+filename+'文件。')
     result = ali.upload_file(file_path='/workdir/'+filename, parent_file_id=remote_folder.file_id)
     print(result)
+    '''旧的linux版删除文件
     result = os.system("rm /workdir/" + filename)
     print(result)
+    '''
+    os.remove('/workdir/'+filename)
+    print('已删除'+filename+'文件。')
+    
 if __name__ == '__main__':
 
     upload_file()
